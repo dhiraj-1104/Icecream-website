29d8b7459d658b2e443719c691fb0e081b425b70
Dhiraj <dhirajchavan@1104>
2025-02-08 16:17:11 +0530

    Header completed

---
+++ main.js
@@ -0,0 +1,32 @@
+// <!--~~~~~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+const navMenu = document.getElementById("nav-menu")
+const navLink = document.querySelectorAll(".nav-link")
+const hamburger = document.getElementById("hamburger")
+const closeicon = document.getElementById("closeicon")
+
+hamburger.addEventListener('click', ()=> {
+    navMenu.classList.toggle("left-[0]")
+})
+
+closeicon.addEventListener('click',()=>{
+    
+    navMenu.classList.toggle("left-[0]")
+})
+
+navLink.forEach(l => {
+   
+    l.addEventListener('click',()=> {
+        
+        navMenu.classList.toggle("left-[0]")
+    })
+})
+
+// <!--~~~~~~~~~~~~~~~~~~~ SWIPER ~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+
+// <!--~~~~~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+
+// <!--~~~~~~~~~~~~~~~~~~~ ACTIVE LINK ~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+
+// <!--~~~~~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+
+// <!--~~~~~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~~~~~~~~~~~~-->
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        firstcolor: "#E10914",
+        cardcolor_1: "#FFF3E1",
+        cardcolor_2: "#FFE4E9",
+        cardcolor_3: "#FFEDBA",
+        cardcolor_4: "#EAD6C0"
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          md:"2rem",
+        }
+      },
+      fontFamily:{
+        Playball: ["Playball","cursive"],
+        Kalnia: ["Kalnia", "serif"],
+        DMSans: ["DM Sans", "serif"]
+      }
+    },
   },
   plugins: [],
 }
